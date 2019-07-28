a821f8a94596844b71d7daade8340a3b8e25e3cb
CodingSoldier <tfz9011@163.com>
2019-07-28 22:18:01 +0800

    00

--- python-hexin/a_/m03_yield.py
+++ python-hexin/a_/m03_yield.py
@@ -17,6 +17,41 @@ def get_url(url):
 
 
 
+# send方法可以传递值进入生成器内部，同时还可以重启生成器执行到下一个yield位置
+def gen_func():
+    data1 = yield "数据111"
+    print("2、内1", data1)
+    data2 = yield "数据22"
+    print("4、内2", data2)
+    return "通过StopIteration.value获取返回值"
+
+gen = gen_func()
+# 通过send发送数据到生成器函数中
+# 发送数据前，必须先启动一次生成器，方式有两种 1、gen.send(None), 2、next(gen)
+# 第一次运行send不能传值
+data1 = gen.send(None)
+print("1、外1", data1)
+
+# 这个参数竟然是穿个了data1
+data2 = gen.send("参数222")
+print("3、外2", data2)
+
+try:
+    data3 = gen.send("参数3333")
+except StopIteration as e:
+    print(e.value)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
